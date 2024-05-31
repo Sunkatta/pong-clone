06734d0b727ba33bf8f0eb572ff1e71fb924339d
Lusian Manolov <lusianmanolov@outlook.com>
2024-05-31 19:29:32 +0300

    Added pause menu

--- src/pong/Assets/Scripts/UI/MainGameUIController.cs
+++ src/pong/Assets/Scripts/UI/MainGameUIController.cs
@@ -6,11 +6,21 @@ public class EndGameController : MonoBehaviour
 {
     public GameManager gameManager;
     public RectTransform endGamePanel;
+    public RectTransform pauseGamePanel;
     public TMP_Text endGameText;
 
-    void Start()
+    private void Start()
     {
         this.gameManager.GameEnded += this.OnGameEnded;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            this.pauseGamePanel.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 
     public void PlayAgain()
@@ -22,6 +32,13 @@ public class EndGameController : MonoBehaviour
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+
+    public void ResumeGame()
+    {
+        this.pauseGamePanel.gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
 
     private void OnGameEnded(PlayerType winner)
